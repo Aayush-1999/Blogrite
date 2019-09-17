0255ba8becbb76a2d8fe52cd026fab4252cf5415
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-09-18 01:04:00 +0530

    Add mongodb new option

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const indexRoute   = require("./routes/index"),
     //   commentRoute = require("./routes/comment");
 
 
-mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
+mongoose.connect(process.env.DATABASEURL,{ useUnifiedTopology: true ,useNewUrlParser:true});
 mongoose.set("useFindAndModify",false);
 mongoose.set("useCreateIndex",true);
 
@@ -24,6 +24,13 @@ app.set("view engine","ejs");
 app.use(methodOverride("_method"));
 
 middleware(app);
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    // res.locals.error  =  req.flash("error");
+    // res.locals.success  =  req.flash("success");
+    next();
+ });
 
 app.use("/",indexRoute);
 app.use("/blog",blogRoute);
