054aded773857ab59c20ab28968d1805fadbec33
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-08 00:51:16 +0530

    Add Preloader

--- public/js/main.js
+++ public/js/main.js
@@ -25,6 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Modal.init(elems,{});
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1000).fadeOut('slow');
+	$('.preloader-wrapper').delay(1000).fadeOut();
+});
 
 //ACCORDIAN
 var acc = document.getElementsByClassName("accordion");
