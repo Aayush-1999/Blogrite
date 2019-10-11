6fc2c5b9a1f1ccf010272fe3e09ecb567775cd10
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-11 23:54:13 +0530

    set time in preloader function

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdowns = document.querySelectorAll('.dropdown-trigger')
-    M.Dropdown.init(dropdowns,{});
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
