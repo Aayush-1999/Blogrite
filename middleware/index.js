900e86a6e6c079ae420e2f85fb90466631eba3fe
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-17 23:32:51 +0530

    refactor path for node_modules dir

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,12 @@
 const express       = require("express");
+    //   path          = require("path"),
       passportSetup = require("./passport/setup");
 
 module.exports = app => {
+    // app.use(express.static(path.join(__dirname+"../public")));
+    // app.use(express.static(path.join(__dirname, '../node_modules/materialize-css/dist')));
+    // app.use(express.static(path.join(__dirname, '../node_modules/animate.css')));
+
     passportSetup(app);
 };
     
