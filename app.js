0c083328f864498025da57347f4189c0034c486f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-19 14:47:52 +0530

    Set path for animate.css, ckeditor, materialize

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ mongoose.set("useFindAndModify",false);
 mongoose.set("useCreateIndex",true);
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs");
 app.use(methodOverride("_method"));
 app.use(flash());
--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,12 @@
 const express       = require("express");
-    //   path          = require("path"),
+      path          = require("path"),
       passportSetup = require("./passport/setup");
 
 module.exports = app => {
-    // app.use(express.static(path.join(__dirname+"../public")));
-    // app.use(express.static(path.join(__dirname, '../node_modules/materialize-css/dist')));
-    // app.use(express.static(path.join(__dirname, '../node_modules/animate.css')));
+    app.use(express.static(path.join(__dirname,"../public")));
+    app.use(express.static(path.join(__dirname,"../node_modules/materialize-css/dist")));
+    app.use(express.static(path.join(__dirname,"../node_modules/animate.css")));
+    app.use(express.static(path.join(__dirname,"../node_modules/@ckeditor/ckeditor5-build-classic/build")));
 
     passportSetup(app);
 };
