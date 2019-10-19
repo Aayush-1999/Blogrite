1ad4cce9b936b70319b2cc6abf72fb17e75b660f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-19 14:48:26 +0530

    Update cache_name

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,18 +18,16 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = ['/offline.html',
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
 );
@@ -37,13 +35,11 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
   // CODELAB: Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
@@ -53,7 +49,6 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
   if (evt.request.mode !== 'navigate') {
     // Not a page navigation, bail.
