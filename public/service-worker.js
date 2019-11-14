5731c5962f9192697fa281148bcb5233427bbc3e
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-11-14 20:31:33 +0530

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,36 +17,11 @@
  */
 'use strict';
 
-<<<<<<< HEAD
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v2';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = ['/offline.html',
-=======
-const CACHE_NAME = 'static-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
-
-const FILES_TO_CACHE = [
-   '/',
-   '/images/icons/Artboard-128.png',
-   '/images/icons/Artboard-144.png',
-   '/images/icons/Artboard-152.png',
-   '/images/icons/Artboard-192.png',
-   '/images/icons/Artboard-256.png',
-   '/images/icons/Artboard-512.png',
-   '/images/bgpattern.png',
-   '/images/fb-icon-1.jpg',
-   '/images/google-icon.svg',
-   '/js/main.js',
-   '/js/single-blog.js',
-   '/stylesheets/style.css',
-   '/css/materialize.min.css',
-   '/js/materialize.min.js',
-   '/offline.html',
-   'https://fonts.googleapis.com/icon?family=Material+Icons',
-   'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
->>>>>>> e8089286ff7fa72b3bbb485a8037785e84a67a31
 ];
 
 self.addEventListener('install', (evt) => {
@@ -73,7 +48,6 @@ self.addEventListener('activate', (evt) => {
   self.clients.claim();
 });
 
-<<<<<<< HEAD
 self.addEventListener('fetch', (evt) => {
   // CODELAB: Add fetch event handler here.
   if (evt.request.mode !== 'navigate') {
@@ -89,54 +63,4 @@ self.addEventListener('fetch', (evt) => {
                 });
           })
   );
-=======
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes("/")) {
-  //   evt.respondWith(
-  //       caches.open(DATA_CACHE_NAME).then((cache) => {
-  //         return fetch(evt.request)
-  //             .then((response) => {
-  //               if (response.status === 200) {
-  //                 cache.put(evt.request.url, response.clone());
-  //               }
-  //               return response;
-  //             }).catch((err) => {
-  //               return cache.match(evt.request);
-  //             });
-  //       }));
-  //   return;
-  // }
-  // evt.respondWith(
-  //   caches.open(CACHE_NAME).then((cache) => {
-  //     return cache.match(evt.request)
-  //       .then((response)=>{
-  //         return response || fetch(evt.request);
-  //       });
-  //   })
-  // );
-  event.respondWith(
-    caches.match(event.request)
-      .then((response)=> {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then((res)=> {
-              return caches.open(DATA_CACHE_NAME)
-                .then((cache)=> {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function (err) {
-              return caches.open(CACHE_NAME)
-                .then((cache)=>{
-                    return cache.match('/offline.html');
-                });
-            });
-        }
-      })
-  );
-  }
->>>>>>> e8089286ff7fa72b3bbb485a8037785e84a67a31
 });
