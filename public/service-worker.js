a0897ecde59134e14b9be326c1f2570912b7f94d
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-01 22:39:18 +0530

    Add complete offline experience

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,31 +1,49 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 'use strict';
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = ['/offline.html',
+const FILES_TO_CACHE = [
+   '/',
+   '/images/icons/Artboard-128.png',
+   '/images/icons/Artboard-144.png',
+   '/images/icons/Artboard-152.png',
+   '/images/icons/Artboard-192.png',
+   '/images/icons/Artboard-256.png',
+   '/images/icons/Artboard-512.png',
+   '/images/bgpattern.png',
+   '/images/fb-icon-1.jpg',
+   '/images/google-icon.svg',
+   '/js/main.js',
+   '/js/single-blog.js',
+   '/stylesheets/style.css',
+   '/css/materialize.min.css',
+   '/js/materialize.min.js',
+   '/offline.html',
+   'https://fonts.googleapis.com/icon?family=Material+Icons',
+   'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
 ];
 
+function cleanResponse(response) {
+  const clonedResponse = response.clone();
+
+  // Not all browsers support the Response.body stream, so fall back to reading
+  // the entire body into memory as a blob.
+  const bodyPromise = 'body' in clonedResponse ?
+    Promise.resolve(clonedResponse.body) :
+    clonedResponse.blob();
+
+  return bodyPromise.then((body) => {
+    // new Response() is happy when passed either a stream or a Blob.
+    return new Response(body, {
+      headers: clonedResponse.headers,
+      status: clonedResponse.status,
+      statusText: clonedResponse.statusText,
+    });
+  });
+}
+
 self.addEventListener('install', (evt) => {
-  // CODELAB: Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
@@ -35,11 +53,10 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  // CODELAB: Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
+        if (key !== CACHE_NAME  && key !== DATA_CACHE_NAME) {
           return caches.delete(key);
         }
       }));
@@ -48,19 +65,126 @@ self.addEventListener('activate', (evt) => {
   self.clients.claim();
 });
 
+// STATIC CHACHE OFFLINE EXPERIENCE
+// self.addEventListener('fetch', (evt) => {
+//   if (evt.request.mode !== 'navigate') {
+//     // Not a page navigation, bail.
+//     return;
+//   }
+//   evt.respondWith(
+//       fetch(evt.request)
+//           .catch(() => {
+//             return caches.open(CACHE_NAME)
+//                 .then((cache) => {
+//                   return cache.match('offline.html');
+//                 });
+//           })
+//   );
+// });
+
+
+//STATIC AND DYNAMIC CACHE OFFLINE EXPERIENCE
 self.addEventListener('fetch', (evt) => {
-  // CODELAB: Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
+  if (evt.request.url.includes('/blog')) {
+    evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+          return fetch(evt.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+                console.log("network response /blog");
+                return response;
+              }).catch((err) => {
+                return cache.match(evt.request)
+                  .then((response)=>{
+                    if(response){
+                      console.log("cache response /blog");
+                        return response;
+                    }
+                    else{
+                      return caches.open(CACHE_NAME)
+                        .then((cache)=>{
+                            return cache.match('/offline.html');
+                        });        
+                    }
+                  })
+              });
+        }));
     return;
   }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
+  // if (evt.request.url.includes('/')) {
+  //   evt.respondWith(
+  //       caches.open(DATA_CACHE_NAME).then((cache) => {
+  //         return fetch(evt.request)
+  //             .then((response) => {
+  //               if (response.status === 200) {
+  //                 cache.put(evt.request.url, response.clone());
+  //               }
+  //               return response;
+  //             }).catch((err) => {
+  //               return cache.match(evt.request)
+  //                 .then((response)=>{
+  //                   if(response){
+  //                     if(response.redirected){
+  //                       return cleanResponse(response);
+  //                     }
+  //                     else{
+  //                       return response;
+  //                     }
+  //                   }
+  //                   else{
+  //                     return caches.open(CACHE_NAME)
+  //                       .then((cache)=>{
+  //                           return cache.match('/offline.html');
+  //                       });        
+  //                   }
+  //                 })
+  //             });
+  //       }));
+  //   return;
+  // }
+  // evt.respondWith(
+  //   caches.open(CACHE_NAME).then((cache) => {
+  //     return cache.match(evt.request)
+  //       .then((response)=>{
+  //         return response || fetch(evt.request);
+  //       });
+  //   })p
+  // );
+
+  else if (evt.request.url.includes('/')) {
+    evt.respondWith(
+       fetch(evt.request)
+        .then((res)=> {
+          return caches.open(DATA_CACHE_NAME)
+            .then((cache)=> {
+              cache.put(evt.request.url, res.clone());
+              console.log("network response /");
+              return res;
+            })
+        })
+        .catch((err)=> {
+          return caches.match(evt.request)
+            .then((response)=> {
+              if (response) {
+                if(response.redirected){
+                  console.log("cache response /");
+                  return cleanResponse(response);
+                }
+                else{
+                  return response;
+                }
+              }
+              else{
+                return caches.open(CACHE_NAME)
+                  .then((cache)=>{
+                    return cache.match('/offline.html');
+                  });
+              }
+            })
+        })
+      )
+    return;
+  }
 });
