d91b7ecd9a0ff0b63bce5b25dde1946a252e9940
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-05 23:06:17 +0530

    Create security.js and add middlewares for csrf and helmet.js

---
+++ middleware/security.js
@@ -0,0 +1,23 @@
+const csrf   = require("csurf"),
+      helmet = require("helmet");
+
+module.exports=app=>{
+    app.use(helmet());
+    
+    // Content Security policy
+    // app.use(helmet.contentSecurityPolicy({
+    //     directives: {
+    //     defaultSrc: ["'self'"],
+    //     styleSrc: ["'self'", 'fonts.googleapis.com/', 'use.fontawesome.com/', "'unsafe-inline'"],
+    //     fontSrc: ["'self'", 'fonts.gstatic.com/', 'use.fontawesome.com/'],
+    //     scriptSrc: ["'self'", 'use.fontawesome.com/'],
+    //     imgSrc: ["'self'", 'lh6.googleusercontent.com/', 'media.licdn.com', 'avatars0.githubusercontent.com', 'platform-lookaside.fbsbx.com'],
+    //     },
+    // }));    
+    
+    app.use(csrf({
+        ignoreMethods:['GET']
+    }));
+
+
+}
