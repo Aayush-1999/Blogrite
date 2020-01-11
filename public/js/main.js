e8d1f2837d659ab775199f298bee2bad2a53c720
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-12 03:28:05 +0530

    del single-blog.js file and moves all its js to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -23,6 +23,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+//ACCORDIAN
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    M.Parallax.init(elems, {});
+});
+
+
 document.addEventListener("DOMContentLoaded", function(){
 	$('.preloader-background').delay(750).fadeOut('slow');
 	$('.preloader-wrapper').delay(750).fadeOut();
